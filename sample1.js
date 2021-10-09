2eebe2c93761cfa9675f97de7d056d6e5376d78e
springkyu <86840763+springkyu@users.noreply.github.com>
2021-10-09 23:57:16 +0900

    [Update]ロゴを1.2秒でフェードアウトする記述

--- sample1.js
+++ sample1.js
@@ -53,3 +53,7 @@ $(window).on('load',function(){
   $("#splash").delay(1500).fadeOut('slow'); //ローディング画面を1.5秒（1500ms）待機してからfadeout
   $("#splash_logo").delay(1200).fadeOut('slow'); //ロゴを1.2秒（1200ms）待機してからfadeout
 });
+
+
+$(window).on('load',function(){
+  $("#splash2-logo").delay(1200).fadeOut('slow'); //ロゴを1.2秒でfadeout
