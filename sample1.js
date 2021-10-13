6749cee68ed2ce6dab7723e74abe76585b54f677
springkyu <86840763+springkyu@users.noreply.github.com>
2021-10-13 23:15:13 +0900

    [Finish]検索/js

--- sample1.js
+++ sample1.js
@@ -125,3 +125,14 @@ $(window).on('load resize', function() {
   Stickyfill.remove(elements);
   }
 });
+
+//　開くボタンを押した時には
+$(".open-btn").click(function () {
+  $("#search-wrap").addClass('panelactive');　//　#search-wrapへpanelactiveクラスを付与
+$('#search-text').focus();　//　テキスト入力のinputにフォーカス
+});
+
+//　閉じるボタンを押した時には
+$(".close-btn").click(function () {
+  $("#search-wrap").removeClass('panelactive');　//　#search-wrapからpanelactiveクラスを除去
+});
