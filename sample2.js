89e78f336751d4ffd4d3d4b3ac6ce2a6525dc553
springkyu <86840763+springkyu@users.noreply.github.com>
2021-11-04 20:10:07 +0900

    コメントアウト

--- sample2.js
+++ sample2.js
@@ -1,34 +1,34 @@
-// 値をグラフに表示させる
+// // 値をグラフに表示させる
 
-Chart.plugins.register({
-  afterDatasetsDraw: function (chart, easing) {
-    var ctx = chart.ctx;
-    chart.data.datasets.forEach(function (dataset, i) {
-      var meta = chart.getDatasetMeta(i);
-      if (!meta.hidden) {
-        meta.data.forEach(function (element, index) {
-          // 値の表示
-          ctx.fillStyle = 'rgb(0, 0, 0,0.8)'; // 文字色
-          var fontSize = 12; // フォントサイズ
-          var fontStyle = 'normal'; // フォントスタイル
-          var fontFamily = 'Arial'; // フォントファミリー
-          ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+// Chart.plugins.register({
+//   afterDatasetsDraw: function (chart, easing) {
+//     var ctx = chart.ctx;
+//     chart.data.datasets.forEach(function (dataset, i) {
+//       var meta = chart.getDatasetMeta(i);
+//       if (!meta.hidden) {
+//         meta.data.forEach(function (element, index) {
+//           // 値の表示
+//           ctx.fillStyle = 'rgb(0, 0, 0,0.8)'; // 文字色
+//           var fontSize = 12; // フォントサイズ
+//           var fontStyle = 'normal'; // フォントスタイル
+//           var fontFamily = 'Arial'; // フォントファミリー
+//           ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
 
-          var dataString = dataset.data[index].toString();
+//           var dataString = dataset.data[index].toString();
 
-          // 値の位置
-          ctx.textAlign = 'center'; // テキスト中央寄せ
-          ctx.textBaseline = 'middle'; // テキストベースラインの位置を中央揃え
+//           // 値の位置
+//           ctx.textAlign = 'center'; // テキスト中央寄せ
+//           ctx.textBaseline = 'middle'; // テキストベースラインの位置を中央揃え
 
-          var padding = 5; // 余白
-          var position = element.tooltipPosition();
-          ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+//           var padding = 5; // 余白
+//           var position = element.tooltipPosition();
+//           ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
 
-        });
-      }
-    });
-  }
-});
+//         });
+//       }
+//     });
+//   }
+// });
 
 
 // // 棒グラフ（縦）
