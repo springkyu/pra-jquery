cd2465a914acfd7c98abf80895897a917a090a4f
springkyu <86840763+springkyu@users.noreply.github.com>
2021-11-05 22:23:29 +0900

    コメントアウト

--- sample1.js
+++ sample1.js
@@ -126,11 +126,11 @@ $(window).on('load resize', function() {
   }
 });
 
-//　開くボタンを押した時には
-$(".open-btn").click(function () {
-  $("#search-wrap").addClass('panelactive');　//　#search-wrapへpanelactiveクラスを付与
-$('#search-text').focus();　//　テキスト入力のinputにフォーカス
-});
+// //　開くボタンを押した時には
+// $(".open-btn").click(function () {
+//   $("#search-wrap").addClass('panelactive');　//　#search-wrapへpanelactiveクラスを付与
+// $('#search-text').focus();　//　テキスト入力のinputにフォーカス
+// });
 
 // //　閉じるボタンを押した時には
 // $(".close-btn").click(function () {
