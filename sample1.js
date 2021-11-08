d922288d38aacc102dfc17e6bf189d26224654cf
springkyu <86840763+springkyu@users.noreply.github.com>
2021-11-08 20:22:42 +0900

    コメントアウト

--- sample1.js
+++ sample1.js
@@ -48,11 +48,11 @@ $('#page-top').click(function () {
 });
 
 
-// logoの表示
-$(window).on('load',function(){
-  $("#splash").delay(1500).fadeOut('slow'); //ローディング画面を1.5秒（1500ms）待機してからfadeout
-  $("#splash_logo").delay(1200).fadeOut('slow'); //ロゴを1.2秒（1200ms）待機してからfadeout
-});
+// // logoの表示
+// $(window).on('load',function(){
+//   $("#splash").delay(1500).fadeOut('slow'); //ローディング画面を1.5秒（1500ms）待機してからfadeout
+//   $("#splash_logo").delay(1200).fadeOut('slow'); //ロゴを1.2秒（1200ms）待機してからfadeout
+// });
 
 
 // $(window).on('load',function(){
