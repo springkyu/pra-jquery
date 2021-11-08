5c3c7cf300d31cfa0473f405794c32ad844ac678
springkyu <86840763+springkyu@users.noreply.github.com>
2021-11-08 20:22:55 +0900

    コメントアウト

--- sample1.js
+++ sample1.js
@@ -38,14 +38,14 @@ $(".openbtn").click(function () {
 
 
 
-// page-topをクリックした際の設定
+// // page-topをクリックした際の設定
 
-$('#page-top').click(function () {
-  $('body,html').animate({
-      scrollTop: 0 // ページトップまでスクロール
-  }, 500); // スクロールの速さ
-  return false; // リンク自体の無効化
-});
+// $('#page-top').click(function () {
+//   $('body,html').animate({
+//       scrollTop: 0 // ページトップまでスクロール
+//   }, 500); // スクロールの速さ
+//   return false; // リンク自体の無効化
+// });
 
 
 // // logoの表示
